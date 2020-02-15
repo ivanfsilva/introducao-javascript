f567da99a4d56186b00d216783fc1c5e97f34517
ivanfsilva <ivanfs27@gmail.com>
2020-02-15 15:46:58 -0300

    Adicionando pacientes na tabela

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -3,7 +3,7 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0 ; i <= pacientes.length ; i++) {
+for(var i = 0 ; i < pacientes.length ; i++) {
     
     var paciente = pacientes[i];
 
@@ -33,14 +33,19 @@ for(var i = 0 ; i <= pacientes.length ; i++) {
     }
 
     if(pesoEhValido && alturaEhValida) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
 }
 
 
 
-console.log(peso);
-console.log(altura);
-console.log(imc);
+
 
